47a5bda4de0d92b9e3665005607a2d8a5016846c
abhishek <abhishekritu810219@gmail.com>
2022-10-24 17:25:31 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+fs.appendFile(path.join(__dirname, 'test', 'test.txt'), 'More data', (err) => {
+    //     if(err) {
+    //         throw err
+    //     } 
+    //     console.log('Data added...') 
+    // })
